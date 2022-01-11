a2d0e7f993bcda8ed1ca8e9e2b15ae73565e9601
Ashu <ash.kelkar.poly@gmail.com>
2022-01-11 20:18:55 +0530

    dummyreact

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import React from 'react';
+import Home from './component/Home';
+import About from './component/About';
+import Contact from './component/Contact';
+import Service from './component/Service';
+// import {Switch, Route} from "react-router-dom";
+const App= () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+      <h1>     welcome page  </h1>
+      <Home />,
+      <About/>,
+      <Contact/>,
+      <Service/>,
+    {/* <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/About" component={About} />
+    <Route exact path="/Service" component={Service} />
+    <Route exact path="/Contact" component={Contact} />
+    </Switch> */}
+    </>
   );
 }
 
-export default App;
+export default App
---
+++ src/component/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () =>{
+  return (
+    <>
+    <h1>welcome Home page</h1>
+    </>
+  );
+}
+
+export default Home;
