e78a1f693ee154d1597c4f337552f956ac6ca5e8
Ashu <ash.kelkar.poly@gmail.com>
2022-01-12 19:32:40 +0530

    dummyreact

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
 // import logo from './logo.svg';
 import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import Navbar from './component/navbar';
 import React from 'react';
 import Home from './component/Home';
 import About from './component/About';
 import Contact from './component/Contact';
 import Service from './component/Service';
-// import {Switch, Route} from "react-router-dom";
+import {Route,Switch} from "react-router-dom";
 const App= () => {
   return (
     <>
-    
-      <h1>     welcome page  </h1>
-      <Home />,
-      <About/>,
-      <Contact/>,
-      <Service/>,
-    {/* <Switch>
+    <Navbar />
+             <center>
+           <h1>     Welcome My Gaming World  </h1>
+            {/* <About />
+            <Contact />
+            <Home />
+            <Service /> */}
+           </center>
+      {/* <Home />,
+      <About />,
+      <Contact />,
+      <Service />, */}
+    <Switch>
     <Route exact path="/" component={Home} />
     <Route exact path="/About" component={About} />
     <Route exact path="/Service" component={Service} />
     <Route exact path="/Contact" component={Contact} />
-    </Switch> */}
+    </Switch>
     </>
   );
 }
 
 export default App
+
